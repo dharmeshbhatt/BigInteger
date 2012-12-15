b590994039941b7f4471595dd131cf59b19faa09
Dharmesh Bhatt <dharmesh.bhatt2@gmail.com>
2012-12-15 16:17:43 -0500

    simplified addition code

--- BigInteger.cpp
+++ BigInteger.cpp
@@ -15,68 +15,37 @@ BigInteger::BigInteger(string input) {
 void BigInteger::add(BigInteger other) {
   int carry = 0;
   
-  int multiplier;
-  if(sign == other.sign) {
-   multiplier = 1;
-  }
-  else {
-    multiplier = -1;
+  // numbers of opposite parity being added
+  // effectively subtraction 
+  if(sign != other.sign) {
+    return subtract(other);
   }
 
-  // v1 will hold the larger number by abs value
-  // v2 will hold the smaller number by abs value
-  vector<int> v1;
-  vector<int> v2;
-  if(nodes.size() > other.nodes.size()) {
-    v1 = nodes; 
-    v2 = other.nodes;
-  }
-  else if(nodes.size() == other.nodes.size()){
-    if(nodes[0] > other.nodes[0]) {
-      v1 = nodes;
-      v2 = other.nodes;
-    }
-    else {
-      v1 = other.nodes;
-      v2 = nodes;
-    }
-  }
-  else {
-    v1 = other.nodes;
-    v2 = nodes;
-  }
-
-  vector::iterator it1;
-  vector::iterator it2;
+  vector<int>::iterator it1 = nodes.begin();
+  vector<int>::iterator it2 = other.nodes.begin();
   vector<int> temp;
-  int carry = 0;
   int temp_sum = 0;
-  int a = 0, b = 0;
+  int carry = 0;
+
   while(true) {
-    if(it1 == v1.end() && it2 == v2.end())
-      break;
- 
-    if(it1 != v1.end()) 
-      a = *it1;
-    else
-      a = 0;
-
-    if(it2 != v2.end())
-      b = *it2;
-    else
-      b = 0;
-    
-    temp_sum = a + b + carry;
-    if(temp_sum > 9)
+    temp_sum = *it1 + *it2 + carry;
+    if(temp_sum > 9) { 
       carry = 1;
+      temp_sum = temp_sum % 10;
+    }
+    else 
+      carry = 0;
+  
+    temp.push_back(temp_sum);
+  }
 
-    temp.push_back(temp_sum%10);
- }   
-
+  // the parity of the number will not have changed
+  // no alteration to the sign
 
   nodes = temp;
-  sign = (sign == other.sign);
   return;
 }
+
+
 
 
